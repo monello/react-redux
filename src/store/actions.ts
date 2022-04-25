1a381cbeace6b22ef247077576c6bbbb91d0c101
Morné Louw <louw.morne@gmail.com>
2022-04-25 19:08:50 +0200

    REDUX: List all the required actions as constants

---
+++ src/store/actions.ts
@@ -0,0 +1,6 @@
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_NEWTODO = "SET_NEWTODO";  // Set the text of the new TODO item
+export const SET_TODOS = "SET_TODOS";      // Set all the Todos, This one will be used when we load all the Todo's from the JSON file
